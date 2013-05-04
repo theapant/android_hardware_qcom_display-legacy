96f4c6b7f7347521a9328049cd429332c094cb23
Anand Pant <theapant@gmail.com>
2013-05-04 11:35:29 -0500

    Revert "display: Use actual kernel headers for all builds"
    This reverts commit ef45ce32f2f4598eee783d7bec7f0c4a84ce44bf.

--- common.mk
+++ common.mk
@@ -32,8 +32,3 @@ endif
 ifeq ($(TARGET_USES_QCOM_BSP),true)
     common_flags += -DQCOM_BSP
 endif
-
-ifeq ($(call is-vendor-board-platform,QCOM),true)
-     common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-     kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
- endif
